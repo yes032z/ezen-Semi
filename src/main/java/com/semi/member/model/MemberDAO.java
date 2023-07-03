a4d93e28af703b43a5a79f23054fc5b5affc6647
ezen <ezen@DESKTOP-O03MT84>
2023-07-03 13:35:20 +0900

    memberinsert

--- src/main/java/com/semi/member/model/MemberDAO.java
+++ src/main/java/com/semi/member/model/MemberDAO.java
@@ -1,5 +1,9 @@
 package com.semi.member.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.semi.db.ConnectionPoolMgr;
 
 public class MemberDAO {
@@ -9,10 +13,32 @@ public class MemberDAO {
 		pool=new ConnectionPoolMgr();
 	}
 	
-	/*
-	 * public insertMember() { }
-	 */
-	
+
+	public int insertMember(MemberVO vo) throws SQLException {
+		Connection con=null;
+		PreparedStatement ps=null;
+
+		try {
+			//1,2
+			con=pool.getConnection();
+
+			//3
+			String sql = "";
+
+			ps = con.prepareStatement(sql);
+			ps.setString(1, vo.getName());
+			
+
+			//4
+			int cnt = ps.executeUpdate();
+			System.out.println("회원가입 결과 cnt = " + cnt + ", 매개변수 vo = " + vo);
+
+			return cnt;
+		}finally {
+			pool.dbClose(ps, con);
+		}
+	}
+
 	
 	
 	
