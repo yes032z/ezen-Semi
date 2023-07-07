9356a6c2717ec6a7b090acf3ee2666abb538eb31
ezen <ezen@DESKTOP-O03MT84>
2023-07-07 17:52:56 +0900

    희진/LOGIN/ZIPCODE

--- src/main/java/com/semi/member/model/MemberDAO.java
+++ src/main/java/com/semi/member/model/MemberDAO.java
@@ -2,6 +2,7 @@ package com.semi.member.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.semi.db.ConnectionPoolMgr;
@@ -13,33 +14,79 @@ public class MemberDAO {
 		pool=new ConnectionPoolMgr();
 	}
 	
-
-	public int insertMember(MemberVO vo) throws SQLException {
+	/**
+	 * 로그인 처리 관련 메서드
+	 * @param userid
+	 * @param pwd
+	 * @return
+	 * @throws SQLException
+	 */
+	public int checkLogin(String id, String pwd) throws SQLException {
 		Connection con=null;
 		PreparedStatement ps=null;
-
+		ResultSet rs=null;
+		
 		try {
 			//1,2
 			con=pool.getConnection();
-
-			//3
-			String sql = "";
-
-			ps = con.prepareStatement(sql);
-			ps.setString(1, vo.getName());
 			
-
+			//3									//outdate is null => 탈퇴하지 않은 회원
+			String sql="select pwd from member where id= ? and outdate is null";
+			ps=con.prepareStatement(sql);
+			ps.setString(1, id);
+			
 			//4
-			int cnt = ps.executeUpdate();
-			System.out.println("회원가입 결과 cnt = " + cnt + ", 매개변수 vo = " + vo);
-
-			return cnt;
+			int result=0; //0 => 로그인실패
+			rs=ps.executeQuery();
+			if(rs.next()) {
+				String dbPwd=rs.getString(1);
+				if(dbPwd.equals(pwd)) {
+					result=MemberService.LOGIN_OK; //로그인 성공
+				}else {
+					result=MemberService.DISAGREE_PWD; //비밀번호 불일치
+				}
+			}else {
+				result=MemberService.NONE_ID; //해당 아이디가 없음
+			}
+			
+			System.out.println("로그인 처리 결과, result="+result
+						+", 매개변수 id="+id+", pwd="+pwd);
+			return result;
 		}finally {
-			pool.dbClose(ps, con);
+			pool.dbClose(rs, ps, con);
 		}
 	}
-
 	
+	  public int insertMember(MemberVO vo) throws SQLException {
+	   Connection con=null; 
+	   PreparedStatement ps=null;
+	  
+	  try { //1,2 con=pool.getConnection();
+	  
+	  //3 
+	 String sql = "";
+	  
+	  ps = con.prepareStatement(sql);
+	  ps.setString(1, vo.getName());
+	  ps.setString(2, vo.getId()); 
+	  ps.setString(3, vo.getPwd());
+	  ps.setInt(4,vo.getFootsize());
+	  ps.setInt(5, vo.getZipno());
+	  ps.setString(6,vo.getDetailaddress());
+	  ps.setString(7, vo.getTel());
+	  ps.setString(8, vo.getEmail());
+	  
+	  //4 
+	  int cnt = ps.executeUpdate();
+	  System.out.println("회원가입 결과 cnt = " + cnt +", 매개변수 vo = " + vo);
+	  
+	  return cnt;
+	  }finally { 
+		  pool.dbClose(ps, con); 
+		  } 
+	  
+	  }
+	 
 	
 	
 	
--- src/main/java/com/semi/member/model/MemberService.java
+++ src/main/java/com/semi/member/model/MemberService.java
@@ -1,5 +1,26 @@
 package com.semi.member.model;
 
-public class MemberService {
+import java.sql.SQLException;
 
+//db작업 이외의 로직을 담는 클래스
+public class MemberService {
+	//아이디 중복확인 관련 상수
+	public static final int USABLE_ID=1; //사용가능
+	public static final int UNUSABLE_ID=2; //이미 존재하는 아이디여서 사용불가
+
+	//로그인 처리 관련 상수
+	public static final int LOGIN_OK=1; //로그인 성공
+	public static final int DISAGREE_PWD=2; //비밀번호 불일치
+	public static final int NONE_ID=3; //해당 아이디가 존재하지 않는다
+	
+	private MemberDAO memberDao;
+	
+	public MemberService() {
+		memberDao=new MemberDAO();
+	}
+	
+	public int checkLogin(String id, String pwd) throws SQLException {
+		return memberDao.checkLogin(id, pwd);
+	}
+	
 }
