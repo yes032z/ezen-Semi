d1f4c021250dc27cdd32ff346985690a0c27f674
pjm02 <pjm02@59.16.23.222>
2023-07-08 15:49:52 +0900

    상품리스트 db작업 - 정민

--- src/main/java/com/semi/product/model/ProductService.java
+++ src/main/java/com/semi/product/model/ProductService.java
@@ -1,5 +1,21 @@
 package com.semi.product.model;
 
-public class ProductService {
+import java.sql.SQLException;
+import java.util.List;
 
+public class ProductService {
+	private ProductDAO productDao;
+	
+	public ProductService(){
+		productDao=new ProductDAO();
+	}
+	
+	public int insertPd(ProductVO vo) throws SQLException {
+		return productDao.insertPd(vo);
+	}
+	
+	public List<ProductVO> selectAllPd() throws SQLException {
+		return productDao.selectAllPd();
+	}
+	
 }
